151967eb12fd025f4c987b3297487e3314010f7a
Fabito02 <fabianojuniorlimaba2@gmail.com>
2025-04-12 18:46:55 -0300

    adicionar cors no backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use('/api', userRoutes);
 
 const port = process.env.PORT || 4000;
